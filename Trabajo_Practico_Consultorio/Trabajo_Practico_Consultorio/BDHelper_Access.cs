e865b9c25d9a89011a63870075dc35f22f9515ce
GerardoSofovich <42209231+GerardoSofovich@users.noreply.github.com>
2018-09-04 21:31:07 +0200

    Add files via upload

---
+++ Trabajo_Practico_Consultorio/Trabajo_Practico_Consultorio/BDHelper_Access.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data.OleDb;
+using System.Data;
+
+namespace Trabajo_Practico_Consultorio
+{
+    class BDHelper_Access
+    {
+        string strConection=@"Provider = Microsoft.Jet.OLEDB.4.0; Data Source = D:\Usuario\Escritorio\Cosas Raras\Progrmacionn\Base de Datosssss\Base de Datos access_TP_PAV\TablasConsultorio_TP_Integrador.mdb";
+        OleDbCommand cmd;
+        OleDbConnection connection;
+
+        public string StrConection { get => StrConection1;}
+        public OleDbCommand Cmd { get => cmd; set => cmd = value; }
+        public OleDbConnection Connection { get => connection; set => connection = value; }
+        public string StrConection1 { get => strConection; set => strConection = value; }
+
+        public DataTable consultaOLDB(string strCmdOldb)
+        {
+            DataTable tablon = new DataTable();
+            connection = new OleDbConnection();
+            cmd = new OleDbCommand();
+            connection.ConnectionString = StrConection;
+            connection.Open();
+            cmd.Connection = connection;
+            cmd.CommandType = CommandType.Text;
+            cmd.CommandText = strCmdOldb;
+            tablon.Load(cmd.ExecuteReader());
+            connection.Close();
+            return tablon;
+        }
+
+
+
+
+
+
+
+        
+    }
+}
---
+++ Trabajo_Practico_Consultorio/Trabajo_Practico_Consultorio/frmLogin.xaml.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+using System.Data;
+
+namespace Trabajo_Practico_Consultorio
+{
+    /// <summary>
+    /// Lógica de interacción para frmLogin.xaml
+    /// </summary>
+    public partial class frmLogin : Window
+    {
+        public frmLogin()
+        {
+            InitializeComponent();
+        }
+
+        private void cmdIngresar_Click(object sender, RoutedEventArgs e)
+        {
+            if (txtUsuario.Text == String.Empty || txtPassword.Text == String.Empty)
+            {
+                MessageBox.Show("Ingrese algo en los campos");
+            }
+            else
+            {
+                BDHelper_Access oDatos = new BDHelper_Access();
+                DataTable tablita = oDatos.consultaOLDB("SELECT * FROM Users WHERE Usuarios = \'" + txtUsuario.Text + "\' AND Password = \'" + txtPassword.Text + "\'");
+                    
+                if (tablita.Rows.Count >0)
+                {
+                    MainWindow frmPrincipal = new MainWindow();
+                    MessageBox.Show("Has ingresado con exito");
+                    frmPrincipal.Show();
+                    this.Close();
+                }
+                else
+                {
+                    MessageBox.Show("Escribio los datos incorrectos");
+                }
+            }
+        }
+    }
+}
