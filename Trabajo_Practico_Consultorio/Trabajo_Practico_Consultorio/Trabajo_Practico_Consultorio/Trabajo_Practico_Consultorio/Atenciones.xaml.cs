546aa2de4c8d0dfd27bd31d3b8f365e0e14dd16a
GerardoSofovich <42209231+GerardoSofovich@users.noreply.github.com>
2018-09-04 22:18:09 +0200

    Add files via upload
    parte2 (?

---
+++ Trabajo_Practico_Consultorio/Trabajo_Practico_Consultorio/Trabajo_Practico_Consultorio/Trabajo_Practico_Consultorio/Atenciones.xaml.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+using System.Data;
+
+namespace Trabajo_Practico_Consultorio
+{
+    /// <summary>
+    /// Lógica de interacción para Atenciones.xaml
+    /// </summary>
+    public partial class Atenciones : Window
+    {
+        public Atenciones()
+        {
+            InitializeComponent();
+        }
+        private void Llenar_combo(ComboBox cbo,Object source,string display,String value)
+        {
+
+        }
+    }
+}
