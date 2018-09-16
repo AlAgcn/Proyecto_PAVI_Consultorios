017d7273dc71b3cde41615622a1c30f1187882b9
GerardoSofovich <42209231+GerardoSofovich@users.noreply.github.com>
2018-09-16 02:13:49 -0300

    Clases del Consultorio para empezar con los DAO

---
+++ AtencionMedica.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProyectoPav.ClasesBussiness
+{
+    class AtencionMedica
+    {
+        public int Id { set;  get; }
+        public int id_HistorialClinico { set; get; }
+        public string Motivo_Consulta { set; get; }
+        public string Revision { set; get; }
+        public string Diagnostico { set; get; }
+        public string Plan_Terapeutico { set; get; }
+        public string Observaciones { set; get; }
+        public DateTime Fecha { set; get; }
+    }
+
+}
