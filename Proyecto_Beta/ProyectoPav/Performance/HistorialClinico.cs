64a8d125efacf4578316b34837efa395bccecaa8
GerardoSofovich <42209231+GerardoSofovich@users.noreply.github.com>
2018-09-26 19:42:45 -0300

    Add files via upload

---
+++ Proyecto_Beta/ProyectoPav/Performance/HistorialClinico.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProyectoPav
+{
+    class HistorialClinico
+    {
+        public int cod { set; get; }
+        public string nombre { set; get; }
+        public string apellido { set; get; }
+        public int dni { set; get; }
+
+    }
+}
