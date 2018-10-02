31f5bd6489582857ec6d38887e9040e18598adfb
GerardoSofovich <42209231+GerardoSofovich@users.noreply.github.com>
2018-10-02 00:32:12 -0300

    Add files via upload

---
+++ copia/frmAtencion.cs
@@ -0,0 +1,77 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Timers;
+
+namespace copia
+{
+    public partial class frmAtencion : Form
+    {
+        string NOM;
+        int HISTO;
+        public frmAtencion(string nom,int histo)
+        {
+            InitializeComponent();
+            NOM = nom;
+            HISTO = histo;
+        }
+
+        private void frmAtencion_Load(object sender, EventArgs e)
+        {
+            lblPaciente.Text = NOM;
+            lblHistorial.Text = HISTO.ToString();
+            gpbAtencion.Text = "Motivos";
+            lblAnterior.Text = "Motivos anteriores";
+            lblActual.Text = "Motivos actuales";
+        }
+
+        private void cmdMotivos_Click(object sender, EventArgs e)
+        {
+            gpbAtencion.Text = "Motivos";
+            lblAnterior.Text = "Motivos anteriores";
+            lblActual.Text = "Motivos actuales";
+        }
+
+        private void cmdRevision_Click(object sender, EventArgs e)
+        {
+            gpbAtencion.Text = "Revision";
+            lblAnterior.Text = "Revision anteriores";
+            lblActual.Text = "Revision actual";
+
+        }
+
+        private void cmdDiagnostico_Click(object sender, EventArgs e)
+        {
+            gpbAtencion.Text = "Diagnostico";
+            lblAnterior.Text = "Diagnostico anteriores";
+            lblActual.Text = "Diagnostico actual";
+        }
+
+        private void cmdPlan_Click(object sender, EventArgs e)
+        {
+            gpbAtencion.Text = "Plan Terapeutico";
+            lblAnterior.Text = "Planes anteriores";
+            lblActual.Text = "Plan actual";
+        }
+
+        private void cmdObservacion_Click(object sender, EventArgs e)
+        {
+            gpbAtencion.Text = "Observacion";
+            lblAnterior.Text = "Observaciones anteriores";
+            lblActual.Text = "Observacion actual";
+        }
+
+        private void cmdSalir_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+
+
+    }
+}
---
+++ copia/frmElegirPaciente.designer.cs
@@ -0,0 +1,188 @@
+﻿namespace copia
+{
+    partial class frmElegirPaciente
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.cmdConsultar = new System.Windows.Forms.Button();
+            this.cmdSalir = new System.Windows.Forms.Button();
+            this.cmdDetalle = new System.Windows.Forms.Button();
+            this.dgvPacientes = new System.Windows.Forms.DataGridView();
+            this.txtNombre = new System.Windows.Forms.TextBox();
+            this.txtHistoria = new System.Windows.Forms.TextBox();
+            this.txtDni = new System.Windows.Forms.TextBox();
+            this.txtApellido = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.dgvPacientes)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // cmdConsultar
+            // 
+            this.cmdConsultar.Location = new System.Drawing.Point(537, 95);
+            this.cmdConsultar.Name = "cmdConsultar";
+            this.cmdConsultar.Size = new System.Drawing.Size(75, 23);
+            this.cmdConsultar.TabIndex = 0;
+            this.cmdConsultar.Text = "Consultar";
+            this.cmdConsultar.UseVisualStyleBackColor = true;
+            this.cmdConsultar.Click += new System.EventHandler(this.cmdConsultar_Click);
+            // 
+            // cmdSalir
+            // 
+            this.cmdSalir.Location = new System.Drawing.Point(566, 340);
+            this.cmdSalir.Name = "cmdSalir";
+            this.cmdSalir.Size = new System.Drawing.Size(75, 23);
+            this.cmdSalir.TabIndex = 1;
+            this.cmdSalir.Text = "Salir";
+            this.cmdSalir.UseVisualStyleBackColor = true;
+            this.cmdSalir.Click += new System.EventHandler(this.cmdSalir_Click);
+            // 
+            // cmdDetalle
+            // 
+            this.cmdDetalle.Location = new System.Drawing.Point(24, 340);
+            this.cmdDetalle.Name = "cmdDetalle";
+            this.cmdDetalle.Size = new System.Drawing.Size(130, 23);
+            this.cmdDetalle.TabIndex = 2;
+            this.cmdDetalle.Text = "Ali hace la magia";
+            this.cmdDetalle.UseVisualStyleBackColor = true;
+            this.cmdDetalle.Click += new System.EventHandler(this.cmdDetalle_Click);
+            // 
+            // dgvPacientes
+            // 
+            this.dgvPacientes.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dgvPacientes.Location = new System.Drawing.Point(24, 150);
+            this.dgvPacientes.Name = "dgvPacientes";
+            this.dgvPacientes.Size = new System.Drawing.Size(588, 150);
+            this.dgvPacientes.TabIndex = 3;
+            // 
+            // txtNombre
+            // 
+            this.txtNombre.Location = new System.Drawing.Point(115, 37);
+            this.txtNombre.Name = "txtNombre";
+            this.txtNombre.Size = new System.Drawing.Size(100, 20);
+            this.txtNombre.TabIndex = 4;
+            // 
+            // txtHistoria
+            // 
+            this.txtHistoria.Location = new System.Drawing.Point(332, 94);
+            this.txtHistoria.Name = "txtHistoria";
+            this.txtHistoria.Size = new System.Drawing.Size(100, 20);
+            this.txtHistoria.TabIndex = 5;
+            // 
+            // txtDni
+            // 
+            this.txtDni.Location = new System.Drawing.Point(332, 37);
+            this.txtDni.Name = "txtDni";
+            this.txtDni.Size = new System.Drawing.Size(100, 20);
+            this.txtDni.TabIndex = 6;
+            // 
+            // txtApellido
+            // 
+            this.txtApellido.Location = new System.Drawing.Point(115, 91);
+            this.txtApellido.Name = "txtApellido";
+            this.txtApellido.Size = new System.Drawing.Size(100, 20);
+            this.txtApellido.TabIndex = 7;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(24, 94);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(89, 13);
+            this.label1.TabIndex = 8;
+            this.label1.Text = "Apellido Paciente";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(24, 43);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(89, 13);
+            this.label2.TabIndex = 9;
+            this.label2.Text = "Nombre Paciente";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(250, 98);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(76, 13);
+            this.label3.TabIndex = 10;
+            this.label3.Text = "Historia Clinica";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(250, 44);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(26, 13);
+            this.label4.TabIndex = 11;
+            this.label4.Text = "DNI";
+            // 
+            // frmElegirPaciente
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(710, 388);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.txtApellido);
+            this.Controls.Add(this.txtDni);
+            this.Controls.Add(this.txtHistoria);
+            this.Controls.Add(this.txtNombre);
+            this.Controls.Add(this.dgvPacientes);
+            this.Controls.Add(this.cmdDetalle);
+            this.Controls.Add(this.cmdSalir);
+            this.Controls.Add(this.cmdConsultar);
+            this.Name = "frmElegirPaciente";
+            this.Text = "frmElegirPaciente";
+            ((System.ComponentModel.ISupportInitialize)(this.dgvPacientes)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button cmdConsultar;
+        private System.Windows.Forms.Button cmdSalir;
+        private System.Windows.Forms.Button cmdDetalle;
+        private System.Windows.Forms.DataGridView dgvPacientes;
+        private System.Windows.Forms.TextBox txtNombre;
+        private System.Windows.Forms.TextBox txtHistoria;
+        private System.Windows.Forms.TextBox txtDni;
+        private System.Windows.Forms.TextBox txtApellido;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+    }
+}
